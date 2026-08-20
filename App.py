10f2845794a4152e523d6a5f2de8a26d3941b246
Hamarb <joshua.bramah@gmail.com>
2026-08-20 12:58:30 +1000

    Update challenge header to include emojis

--- App.py
+++ App.py
@@ -134,7 +134,7 @@ SEGMENT_URL = get_segment_url()
 
 # --- SPLIT HEADER WITH FEMALE & MALE CYCLISTS ---
 st.markdown("<h1 style='text-align: center;'>🚴‍♀️ 🚴‍♂️ Bulimba Roadies</h1>", unsafe_allow_html=True)
-st.markdown("<h2 style='text-align: center;'>Monthly Challenge</h2>", unsafe_allow_html=True)
+st.markdown("<h1 style='text-align: center;'>Monthly Challenge 🚴‍♂️ 🚴‍♀️</h2>", unsafe_allow_html=True)
 st.markdown(f"**The active challenge segment is:** [{SEGMENT_URL}]({SEGMENT_URL})")
 
 # --- TABS ---
